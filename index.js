ed508ed9cdbc20a54f92e9d39471fa140856a475
Takayoshi Sato <webmaster@smilemac.com>
2014-01-17 10:45:08 +0900

    Add contents

---
+++ index.js
@@ -0,0 +1,9 @@
+function menu1() {
+	document.getElementById("submenu-1").innerHTML = 'MENU1-1';
+	document.getElementById("submenu-2").innerHTML = 'MENU1-2';
+}
+
+function menu2() {
+	document.getElementById("submenu-1").innerHTML = 'MENU2-1';
+	document.getElementById("submenu-2").innerHTML = 'MENU2-2';
+}
